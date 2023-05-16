d371e6675a1b0e4d2857d855b79f5490948e68c4
Arthur Oechsler <arthuroechsler@outlook.com>
2023-05-16 00:52:27 -0300

    começando questao 4 do trabalho

---
+++ questao04trabalho.py
@@ -0,0 +1,58 @@
+"""
+
+******************************
+1.Cadastrar Peça
+2.Consultar Peça
+    1) Consultar Todas as Peças
+    2) Consulta Peças por Código
+    3) Consulta Peças por Fabricante
+    4) Retornar
+3.Remover Peça
+4.Sair
+*******************************
+
+---------------------------------------------------------------------------------------------
+
+Deve-se codificar uma função cadastrarPeca (código) (EXIGÊNCIA 1);
+
+    Essa função recebe como parâmetro um código exclusivo para cada peça cadastrado
+    (DICA: utilize um contador como parâmetro)
+
+    Dentro da função perguntar o nome da peça;
+
+    Dentro da função perguntar o fabricante da peça;
+
+    Dentro da função perguntar o valor da peça
+
+Cada peça cadastrada deve ter os seus dados armazenados num
+DICIONÁRIO (DICA: Conferir material escrito da p. 22 até p24  da AULA 06)
+
+---------------------------------------------------------------------------------------------
+
+
+Deve-se codificar uma função consultarPeca(EXIGÊNCIA 2);
+
+    Dentro da função ter um menu com as seguintes opções:
+
+        Consultar Todas as Peças
+
+        Consultar Peças por Código
+
+        Consultar Peças por Fabricante
+
+        Retornar
+
+
+
+---------------------------------------------------------------------------------------------
+
+
+
+Deve-se codificar uma função chamada removerPeca (EXIGÊNCIA 3);
+
+    Dentro da função perguntar qual o código do produto que se deseja remover do
+    cadastro (da lista de dicionário)
+
+
+---------------------------------------------------------------------------------------------
+"""
